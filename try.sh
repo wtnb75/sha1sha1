ad37a63bec19c29d375a8473bc28aac6a6de6498
Takashi WATANABE <wtnb75@gmail.com>
2016-12-05 23:03:36 +0900

    add ignore

--- try.sh
+++ try.sh
@@ -3,7 +3,7 @@
 seed=${1-da39a3ee5e6b4b0d3255bfef95601890afd80709}
 seedlen=$(echo $(echo -n ${seed} | wc -c))
 
-go build
+[ -x sha1sha1 ] || go build
 for i in $(seq 8); do
   ns=$(echo ${seed} | cut -c1-$(expr $seedlen - $i))
   ./sha1sha1 -prefix $ns -len $i > ${seed}.${i}.txt
